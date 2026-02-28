825f563d5de2aea8f9b92adb3cc3009bfbe6041c
surtr1 <2363103622@qq.com>
2026-02-28 11:52:48 +0800

    离散傅里叶变换

---
+++ Polynomial/dft && idft.cpp
@@ -0,0 +1,66 @@
+/**
+ * 离散傅里叶变换 dft 与其逆变换 idft
+ */
+
+vector<int> rev;
+template <int P>
+vector<MInt<P>> roots{0, 1};
+template <int P> constexpr MInt<P> findPrimitiveRoot() {
+    MInt<P> i = 2;
+    int k = __builtin_ctz(P - 1);
+    while (true) {
+        if (power(i, (P - 1) / 2) != 1) {
+            break;
+        }
+        i += 1;
+    }
+    return power(i, (P - 1) >> k);
+}
+template <int P> constexpr MInt<P> primitiveRoot = findPrimitiveRoot<P>();
+template <> constexpr MInt<998244353> primitiveRoot<998244353>{31};
+template <int P> constexpr void dft(vector<MInt<P>> &a) { // 离散傅里叶变换
+    int n = a.size();
+    if (int(rev.size()) != n) {
+        int k = __builtin_ctz(n) - 1;
+        rev.resize(n);
+        for (int i = 0; i < n; i++) {
+            rev[i] = rev[i >> 1] >> 1 | (i & 1) << k;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (rev[i] < i) {
+            swap(a[i], a[rev[i]]);
+        }
+    }
+    if (roots<P>.size() < n) {
+        int k = __builtin_ctz(roots<P>.size());
+        roots<P>.resize(n);
+        while ((1 << k) < n) {
+            auto e = power(primitiveRoot<P>, 1 << (__builtin_ctz(P - 1) - k - 1));
+            for (int i = 1 << (k - 1); i < (1 << k); i++) {
+                roots<P>[2 * i] = roots<P>[i];
+                roots<P>[2 * i + 1] = roots<P>[i] * e;
+            }
+            k++;
+        }
+    }
+    for (int k = 1; k < n; k *= 2) {
+        for (int i = 0; i < n; i += 2 * k) {
+            for (int j = 0; j < k; j++) {
+                MInt<P> u = a[i + j];
+                MInt<P> v = a[i + j + k] * roots<P>[k + j];
+                a[i + j] = u + v;
+                a[i + j + k] = u - v;
+            }
+        }
+    }
+}
+template<int P> constexpr voididft(vector<MInt<P>> &a) { // 逆变换
+    int n = a.size();
+    reverse(a.begin() + 1, a.end());
+    dft(a);
+    MInt<P> inv = (1 - P) / n;
+    for (int i = 0; i < n; i++) {
+        a[i] *= inv;
+    }
+}
